1d4b2c7974e0dde3f4e0d9ced373c59d84889939
Marius Rochon <mrochon@microsoft.com>
2021-02-11 12:47:13 -0800

    Add project files.

---
+++ B2BSelfSignup/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using B2BSelfSignup.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web;
+using Microsoft.JSInterop.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace B2BSelfSignup.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ITokenAcquisition _tokenAcquisition;
+
+        public HomeController(ILogger<HomeController> logger, ITokenAcquisition tokenAcquisition)
+        {
+            _logger = logger;
+            _tokenAcquisition = tokenAcquisition;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            _logger.LogTrace("Home.Index starting");
+            var token = await _tokenAcquisition.GetAccessTokenForAppAsync("https://graph.microsoft.com/.default", "meraridom.com");
+            var http = new HttpClient();
+            http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            // Does the user already exist? Maybe I don't need to
+
+            // Invite user
+            var email = User.FindFirst("preferred_username").Value;
+            var invitation = new
+            {
+                invitedUserEmailAddress = email,
+                inviteRedirectUrl = "https://microsoft.com"
+            };
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://graph.microsoft.com/v1.0/invitations")
+            {
+                Content = new StringContent(JsonSerializer.Serialize(invitation), Encoding.UTF8, "application/json")
+            };
+            var resp = await http.SendAsync(request);
+            if (resp.IsSuccessStatusCode)
+            {
+                _logger.LogInformation($"User {email} successfully invited");
+                var json = await resp.Content.ReadAsStringAsync();
+                //var newId = JsonDocument.Parse(json).RootElement.GetProperty("invitedUser").GetProperty("id").GetString();
+                var redeemUrl = JsonDocument.Parse(json).RootElement.GetProperty("inviteRedeemUrl").GetString();
+                Response.Redirect(redeemUrl);
+            } else
+            {
+                Response.Redirect("/error");
+            }
+            _logger.LogTrace("Home.Index existing");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ B2BSelfSignup/Startup.cs
@@ -0,0 +1,108 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B2BSelfSignup
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // Use https://login.microsoftonline.com/fbgpg.com/v2.0/.well-known/openid-configuration to discover tenantId
+
+        private readonly string[] ValidTenants =
+        {
+            "d5cf3ff0-9668-4c65-af0b-054ca6cf6eaa", // masterbrand.com & masterbrandcabinets.com
+            "ce505d35-51b1-4cb8-8311-1bc180cb137a", // fbgpg.com & moen.com
+            "72f988bf-86f1-41af-91ab-2d7cd011db47" // microsoft.com
+        };
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"))
+                .EnableTokenAcquisitionToCallDownstreamApi()
+                .AddInMemoryTokenCaches();
+            services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                var currDelegate = options.Events.OnTokenValidated;
+                options.Events.OnTokenValidated = async (ctx) =>
+                {
+                    var tid = ctx.Principal.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+                    if (!ValidTenants.Contains(tid))
+                    {
+                        ctx.Fail(new Exception("Unauthorized"));
+                    }
+                    if (currDelegate != null)
+                        await currDelegate.Invoke(ctx);
+                };
+                options.Events.OnAuthenticationFailed = async (ctx) =>
+                {
+                    ctx.Response.Redirect($"/Error?msg={Base64UrlEncoder.Encode(ctx.Exception.Message)}");
+                    ctx.HandleResponse(); // Suppress the exception
+                    await Task.CompletedTask;
+                };
+            });
+
+            services.AddControllersWithViews(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            });
+            services.AddRazorPages()
+                 .AddMicrosoftIdentityUI();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
