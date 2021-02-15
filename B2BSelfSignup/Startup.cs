0e2a5b01197792d7426292c544fa1c0083270f81
Marius Rochon <mrochon@microsoft.com>
2021-02-14 21:58:30 -0800

    Added AppInsights

--- B2BSelfSignup/Startup.cs
+++ B2BSelfSignup/Startup.cs
@@ -80,6 +80,7 @@ namespace B2BSelfSignup
             });
             services.AddRazorPages()
                  .AddMicrosoftIdentityUI();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
